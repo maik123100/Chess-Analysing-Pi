fba6cb0a6fdef803de0021fcfe4d9181f5d5e486
Maik <maik.wesselkock@rwth-aachen.de>
2024-04-05 12:39:24 +0200

    fixed structured

--- ChessDB_updating_service.py
+++ ChessDB_updating_service.py
@@ -98,7 +98,7 @@ def push_games_to_db(games):
         # Prepare SQL query to INSERT a record into the database.
         sql = """INSERT INTO games(white_username, white_rating, black_username, black_rating, time_control, pgn, win)
                  VALUES (%s, %s, %s, %s, %s, %s, %s)"""
-        values = (game['white']['username'], game['white']['rating'], game['black']['username'], game['black']['rating'], game['time_control'], ''.join(str(move) for move in games[0]['structured_pgn']['moves']), game['win'])
+        values = (game['white']['username'], game['white']['rating'], game['black']['username'], game['black']['rating'], game['time_control'], ''.join(str(move) for move in games[0]['structured_pgn']['moves']), game['structured_pgn']['win'])
 
         try:
             # Execute the SQL command
