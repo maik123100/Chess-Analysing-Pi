f763d113d709e3ef438d260a4b75ea157fb8de94
Maik <maik.wesselkock@rwth-aachen.de>
2024-08-06 21:52:55 +0200

    PyPi stockfish

--- Chess_analyzing_service.py
+++ Chess_analyzing_service.py
@@ -1,54 +1,7 @@
-import subprocess
-from time import sleep
-import psycopg2
-import os
-from dotenv import load_dotenv
+from stockfish import Stockfish
 
-stockfishPath='../Stockfish/src/stockfish'
+sf=Stockfish(path="~/Projects/Stockfish/src/stockfish",depth=21)
 
-def runStockfishOnFen(fen, depth, threads, path):
-    process = subprocess.Popen([path],
-                               stdin=subprocess.PIPE,
-                               stderr=subprocess.PIPE,
-                               stdout=subprocess.PIPE,
-                               text=True)
-    sleep(0.1)
-    
-    # Send commands to Stockfish and print them
-    commands = [
-        "uci\n",
-        f"setoption name Threads value {threads}\n",
-        f"position fen {fen}\n",
-        f"go depth {depth}\n",
-        "quit\n"
-    ]
-    
-    for command in commands:
-        print(f"Sending command: {command.strip()}")
-        process.stdin.write(command)
-        process.stdin.flush()
-        
-        if command==f"position fen {fen}\n" or command==f"go depth {depth}\n":
-            continue
-
-        # Capture and print output after sending each command
-        while True:
-            output = process.stdout.readline()
-            if output == '' and process.poll() is not None:
-                break
-            if output:
-                print(output.strip())
-            if "uciok" in output or "bestmove" in output:
-                break
-    
-    error = process.stderr.read()
-    if error:
-        print(f"Encountered error: {error.strip()}")
-    
-    return output
-
-if __name__=="__main__":
-    fen="2kr4/p7/bbp3pp/5p2/2P5/5P2/1P2QNNq/2R2K2 b - - 1 31"
-    depth=21
-    threads=4
-    print(runStockfishOnFen(fen,depth,threads,stockfishPath))
+if sf.is_fen_valid("r4r1k/ppp3p1/5n1q/3P4/3Q1p1P/2NB2PN/PPP2P2/R4RK1 b - - 0 18"):
+    sf.set_fen_position("r4r1k/ppp3p1/5n1q/3P4/3Q1p1P/2NB2PN/PPP2P2/R4RK1 b - - 0 18")
+    print(sf.get_best_move())
