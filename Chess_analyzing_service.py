3eba15567639814f10e05254174ec7b8a8ddb935
Maik <maik.wesselkock@rwth-aachen.de>
2024-08-06 21:54:13 +0200

    ~=>..

--- Chess_analyzing_service.py
+++ Chess_analyzing_service.py
@@ -1,6 +1,6 @@
 from stockfish import Stockfish
 
-sf=Stockfish(path="~/Projects/Stockfish/src/stockfish",depth=21)
+sf=Stockfish(path="../Stockfish/src/stockfish",depth=21)
 
 if sf.is_fen_valid("r4r1k/ppp3p1/5n1q/3P4/3Q1p1P/2NB2PN/PPP2P2/R4RK1 b - - 0 18"):
     sf.set_fen_position("r4r1k/ppp3p1/5n1q/3P4/3Q1p1P/2NB2PN/PPP2P2/R4RK1 b - - 0 18")
