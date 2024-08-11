c1d7f6981256b4d73903961137077fd0e6f22176
Maik <maik.wesselkock@rwth-aachen.de>
2024-08-11 17:11:14 +0200

    promotion matching correction

--- Chess_analyzing_service.py
+++ Chess_analyzing_service.py
@@ -112,7 +112,7 @@ if __name__=="__main__":
     games=getGamesFromDB()
     for game in games:
         print("Game:",game["uuid"])
-        moves = re.findall(r'\b(?:[a-h][1-8]|O-O(?:-O)?|[NBRQK]?[a-h]?[1-8]?[x-]?[a-h][1-8](?:=[NBRQ])?[+#]?)\b', game["pgn"])
+        moves = re.findall(r'\b(?:[a-h][1-8](?:=[NBRQ])?|O-O(?:-O)?|[NBRQK]?[a-h]?[1-8]?[x-]?[a-h][1-8](?:=[NBRQ])?[+#]?)\b', game["pgn"])
         try:
             fens = getFensFromMoveList(moves)
             for fen in fens:
